41e32611e4b70720828fee496b956271544aea3b
drathon <kenan3507@gmail.com>
2026-07-18 05:56:54 +0300

    Prevent connected notification refresh

--- vpnLib/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
+++ vpnLib/src/main/java/de/blinkt/openvpn/core/OpenVPNService.java
@@ -1282,7 +1282,7 @@ public class OpenVPNService extends VpnService implements StateListener, Callbac
                     humanReadableByteCount(diffOut / OpenVPNManagement.mBytecountInterval, true, getResources()));
 
 
-            showNotification(netstat, null, NOTIFICATION_CHANNEL_BG_ID, mConnecttime, LEVEL_CONNECTED, null);
+            //showNotification(netstat, null, NOTIFICATION_CHANNEL_BG_ID, mConnecttime, LEVEL_CONNECTED, null);
             // byteIn = String.format("↓%2$s", getString(R.string.statusline_bytecount),
             //         humanReadableByteCount(in, false, getResources())) + " - " + humanReadableByteCount(diffIn / OpenVPNManagement.mBytecountInterval, false, getResources()) + "/s";
             // byteOut = String.format("↑%2$s", getString(R.string.statusline_bytecount),
